354209152207cb3c199e66f33eabe97bc1fada85
Bryan Audiffred <baudiffred@presonus.com>
2021-04-15 15:25:09 -0400

    updated SOC init based on NXP SDK

--- soc/arm/nxp_imx/mimx8mn6_m7/soc.c
+++ soc/arm/nxp_imx/mimx8mn6_m7/soc.c
@@ -14,123 +14,257 @@
 
 #include <dt-bindings/rdc/imx_rdc.h>
 
-/* OSC/PLL is already initialized by ROM and Cortex-A53 (u-boot) */
+/*
+ * ERR050362: TCM: AXI2AHB cannot handle partial write and causes redundant write 
+ * operations to TCM
+ */
+#define GPV5_BASE_ADDR      (0x32500000)
+#define FORCE_INCR_OFFSET   (0x4044)
+#define FORCE_INCR_BIT_MASK (0x2)
+
 static void SOC_RdcInit(void)
 {
-	/* Move M7 core to specific RDC domain 1 */
-	rdc_domain_assignment_t assignment = {0};
+    /* Move M7 core to specific RDC domain 1 */
+    rdc_domain_assignment_t assignment = {0};
 
-	assignment.domainId = M4_DOMAIN_ID;
-	RDC_SetMasterDomainAssignment(RDC, kRDC_Master_M7, &assignment);
+    assignment.domainId = 1;
+    RDC_SetMasterDomainAssignment(RDC, kRDC_Master_M7, &assignment);
 
-	/*
-	 * The M7 core is running at domain 1, enable clock gate for
-	 * Iomux to run at domain 1.
-	 */
-	CLOCK_EnableClock(kCLOCK_Iomux);
-	CLOCK_EnableClock(kCLOCK_Ipmux1);
-	CLOCK_EnableClock(kCLOCK_Ipmux2);
-	CLOCK_EnableClock(kCLOCK_Ipmux3);
-	CLOCK_EnableClock(kCLOCK_Ipmux4);
+    /*
+     *  The M7 core is running at domain 1, enable clock gate for Iomux to run at domain 1.
+     */
+    CLOCK_EnableClock(kCLOCK_Iomux);
 
-	CLOCK_EnableClock(kCLOCK_Qspi);
-
-	/*
-	 * The M7 core is running at domain 1, enable the PLL clock sources
-	 * to domain 1.
-	 */
-	/* Enable SysPLL1 to Domain 1 */
-	CLOCK_ControlGate(kCLOCK_SysPll1Gate, kCLOCK_ClockNeededAll);
-	/* Enable SysPLL2 to Domain 1 */
-	CLOCK_ControlGate(kCLOCK_SysPll2Gate, kCLOCK_ClockNeededAll);
-	/* Enable SysPLL3 to Domain 1 */
-	CLOCK_ControlGate(kCLOCK_SysPll3Gate, kCLOCK_ClockNeededAll);
-	/* Enable AudioPLL1 to Domain 1 */
-	CLOCK_ControlGate(kCLOCK_AudioPll1Gate, kCLOCK_ClockNeededAll);
-	/* Enable AudioPLL2 to Domain 1 */
-	CLOCK_ControlGate(kCLOCK_AudioPll2Gate, kCLOCK_ClockNeededAll);
-	/* Enable VideoPLL1 to Domain 1 */
-	CLOCK_ControlGate(kCLOCK_VideoPll1Gate, kCLOCK_ClockNeededAll);
+    CLOCK_EnableClock(kCLOCK_Ipmux1);
+    CLOCK_EnableClock(kCLOCK_Ipmux2);
+    CLOCK_EnableClock(kCLOCK_Ipmux3);
+    CLOCK_EnableClock(kCLOCK_Ipmux4);
+    /*
+     *  The M7 core is running at domain 1, enable the QSPI clock sources to domain 1 for flash target.
+     */
+#if defined(FLASH_TARGET)
+    CLOCK_EnableClock(kCLOCK_Qspi);
+#endif
+    /*
+     *  The M7 core is running at domain 1, enable the PLL clock sources to domain 1.
+     */
+    CLOCK_ControlGate(kCLOCK_SysPll1Gate, kCLOCK_ClockNeededAll);   /* Enabel SysPLL1 to Domain 1 */
+    CLOCK_ControlGate(kCLOCK_SysPll2Gate, kCLOCK_ClockNeededAll);   /* Enable SysPLL2 to Domain 1 */
+    CLOCK_ControlGate(kCLOCK_SysPll3Gate, kCLOCK_ClockNeededAll);   /* Enable SysPLL3 to Domain 1 */
+    CLOCK_ControlGate(kCLOCK_AudioPll1Gate, kCLOCK_ClockNeededAll); /* Enable AudioPLL1 to Domain 1 */
+    CLOCK_ControlGate(kCLOCK_AudioPll2Gate, kCLOCK_ClockNeededAll); /* Enable AudioPLL2 to Domain 1 */
+    CLOCK_ControlGate(kCLOCK_VideoPll1Gate, kCLOCK_ClockNeededAll); /* Enable VideoPLL1 to Domain 1 */
 }
 
-/* AUDIO PLL1 configuration */
-static const ccm_analog_frac_pll_config_t g_audioPll1Config = {
-	.refSel  = kANALOG_PllRefOsc24M, /* PLL reference OSC24M */
-	.mainDiv = 655U,
-	.dsm     = 23593U,
-	.preDiv  = 5U,
-	.postDiv = 2U, /* AUDIO PLL1 frequency  = 786432000HZ */
+const ccm_analog_frac_pll_config_t g_audioPll1Config = {
+    .refSel  = kANALOG_PllRefOsc24M, /*!< PLL reference OSC24M */
+    .mainDiv = 262U,
+    .dsm     = 9437U,
+    .preDiv  = 2U,
+    .postDiv = 3U, /*!< AUDIO PLL1 frequency  = 393215996HZ */
 };
 
 /* AUDIO PLL2 configuration */
-static const ccm_analog_frac_pll_config_t g_audioPll2Config = {
-	.refSel  = kANALOG_PllRefOsc24M, /* PLL reference OSC24M */
-	.mainDiv = 301U,
-	.dsm     = 3670U,
-	.preDiv  = 5U,
-	.postDiv = 1U, /* AUDIO PLL2 frequency  = 722534399HZ */
+const ccm_analog_frac_pll_config_t g_audioPll2Config = {
+    .refSel  = kANALOG_PllRefOsc24M, /*!< PLL reference OSC24M */
+    .mainDiv = 361U,
+    .dsm     = 17511U,
+    .preDiv  = 3U,
+    .postDiv = 3U, /*!< AUDIO PLL2 frequency  = 361267197HZ */
+};
+
+/* Integer PLLs: Fout = (mainDiv * refSel) / (preDiv * 2^ postDiv) */
+/* SYSTEM PLL1 configuration */
+const ccm_analog_integer_pll_config_t g_sysPll1Config = {
+    .refSel  = kANALOG_PllRefOsc24M, /*!< PLL reference OSC24M */
+    .mainDiv = 400U,
+    .preDiv  = 3U,
+    .postDiv = 2U, /*!< SYSTEM PLL1 frequency  = 800MHZ */
+};
+
+/* SYSTEM PLL2 configuration */
+const ccm_analog_integer_pll_config_t g_sysPll2Config = {
+    .refSel  = kANALOG_PllRefOsc24M, /*!< PLL reference OSC24M */
+    .mainDiv = 250U,
+    .preDiv  = 3U,
+    .postDiv = 1U, /*!< SYSTEM PLL2 frequency  = 1000MHZ */
+};
+
+/* SYSTEM PLL3 configuration */
+const ccm_analog_integer_pll_config_t g_sysPll3Config = {
+    .refSel  = kANALOG_PllRefOsc24M, /*!< PLL reference OSC24M */
+    .mainDiv = 300,
+    .preDiv  = 3U,
+    .postDiv = 2U, /*!< SYSTEM PLL3 frequency  = 600MHZ */
 };
 
 static void SOC_ClockInit(void)
 {
-	/*
-	 * Switch AHB NOC root to 24M first in order to configure
-	 * the SYSTEM PLL1
-	 */
-	CLOCK_SetRootMux(kCLOCK_RootAhb, kCLOCK_AhbRootmuxOsc24M);
+    /* * The following steps just show how to configure the PLL clock sources using the clock driver on M7 core side .
+     * Please note that the ROM has already configured the SYSTEM PLL1 to 800Mhz when power up the SOC, meanwhile A core
+     * would enable the Div output for SYSTEM PLL1 & PLL2 by U-Boot.
+     * Therefore, there is no need to configure the system PLL again on M7 side, otherwise it would have a risk to make
+     * the SOC hang.
+     */
 
-	/*
-	 * Switch AXI M7 root to 24M first in order to configure
-	 * the SYSTEM PLL2
-	 */
-	CLOCK_SetRootMux(kCLOCK_RootM7, kCLOCK_M7RootmuxOsc24M);
+    /* switch AHB NOC root to 24M first in order to configure the SYSTEM PLL1. */
+    CLOCK_SetRootMux(kCLOCK_RootAhb, kCLOCK_AhbRootmuxOsc24M);
 
-	/* Init AUDIO PLL1 to run at 786432000HZ */
-	CLOCK_InitAudioPll1(&g_audioPll1Config);
-	/* Init AUDIO PLL2 to run at 722534399HZ */
-	CLOCK_InitAudioPll2(&g_audioPll2Config);
+    /* switch AXI M7 root to 24M first in order to configure the SYSTEM PLL2. */
+    CLOCK_SetRootMux(kCLOCK_RootM7, kCLOCK_M7RootmuxOsc24M);
 
-	CLOCK_SetRootDivider(kCLOCK_RootM7, 1U, 2U);
-	/* Switch cortex-m7 to SYSTEM PLL1 */
-	CLOCK_SetRootMux(kCLOCK_RootM7, kCLOCK_M7RootmuxSysPll1);
+    /* Init Audio PLL1/Audio PLL2 */
+    CLOCK_InitAudioPll1(&g_audioPll1Config); /* init AUDIO PLL1 run at 393215996HZ */
+    CLOCK_InitAudioPll2(&g_audioPll2Config); /* init AUDIO PLL2 run at 361267197HZ */
 
-	CLOCK_SetRootDivider(kCLOCK_RootAhb, 1U, 1U);
-	/* Switch AHB to SYSTEM PLL1 DIV6 = 133MHZ */
-	CLOCK_SetRootMux(kCLOCK_RootAhb, kCLOCK_AhbRootmuxSysPll1Div6);
+    CLOCK_SetRootDivider(kCLOCK_RootM7, 1U, 1U);              /* Set M7 root clock freq to 600M / 1 = 600M */
+    CLOCK_SetRootMux(kCLOCK_RootM7, kCLOCK_M7RootmuxSysPll3); /* switch cortex-m7 to SYSTEM PLL3 */
 
-	/* Set root clock to 800MHZ/ 2= 400MHZ */
-	CLOCK_SetRootDivider(kCLOCK_RootAudioAhb, 1U, 2U);
-	/* switch AUDIO AHB to SYSTEM PLL1 */
-	CLOCK_SetRootMux(kCLOCK_RootAudioAhb, kCLOCK_AudioAhbRootmuxSysPll1);
+    CLOCK_SetRootDivider(kCLOCK_RootAhb, 1U, 1U);                   /* Set root clock freq to 133M / 1= 133MHZ */
+    CLOCK_SetRootMux(kCLOCK_RootAhb, kCLOCK_AhbRootmuxSysPll1Div6); /* switch AHB to SYSTEM PLL1 DIV6 */
 
-	/* Set UART source to SysPLL1 Div10 80MHZ */
-	CLOCK_SetRootMux(kCLOCK_RootUart4, kCLOCK_UartRootmuxSysPll1Div10);
-	/* Set root clock to 80MHZ/ 1= 80MHZ */
-	CLOCK_SetRootDivider(kCLOCK_RootUart4, 1U, 1U);
+    CLOCK_SetRootDivider(kCLOCK_RootAudioAhb, 1U, 2U);                    /* Set root clock freq to 800MHZ/ 2= 400MHZ*/
+    CLOCK_SetRootMux(kCLOCK_RootAudioAhb, kCLOCK_AudioAhbRootmuxSysPll1); /* switch AUDIO AHB to SYSTEM PLL1 */
 
-	/* Enable RDC clock */
-	CLOCK_EnableClock(kCLOCK_Rdc);
+    CLOCK_SetRootDivider(kCLOCK_RootUart4, 1U, 1U);                     /* Set root clock freq to 80MHZ/ 1= 80MHZ */
+    CLOCK_SetRootMux(kCLOCK_RootUart4, kCLOCK_UartRootmuxSysPll1Div10); /* Set UART source to SysPLL1 Div10 80MHZ */
 
-	/*
-	 * The purpose to enable the following modules clock is to make
-	 * sure the M7 core could work normally when A53 core
-	 * enters the low power state
-	 */
-	CLOCK_EnableClock(kCLOCK_Sim_display);
-	CLOCK_EnableClock(kCLOCK_Sim_m);
-	CLOCK_EnableClock(kCLOCK_Sim_main);
-	CLOCK_EnableClock(kCLOCK_Sim_s);
-	CLOCK_EnableClock(kCLOCK_Sim_wakeup);
-	CLOCK_EnableClock(kCLOCK_Debug);
-	CLOCK_EnableClock(kCLOCK_Dram);
-	CLOCK_EnableClock(kCLOCK_Sec_Debug);
+    CLOCK_EnableClock(kCLOCK_Rdc);   /* Enable RDC clock */
+    CLOCK_EnableClock(kCLOCK_Ocram); /* Enable Ocram clock */
 
-	CLOCK_EnableClock(kCLOCK_Uart4);
+    /* The purpose to enable the following modules clock is to make sure the M7 core could work normally when A53 core
+     * enters the low power status.*/
+    CLOCK_EnableClock(kCLOCK_Sim_display);
+    CLOCK_EnableClock(kCLOCK_Sim_m);
+    CLOCK_EnableClock(kCLOCK_Sim_main);
+    CLOCK_EnableClock(kCLOCK_Sim_s);
+    CLOCK_EnableClock(kCLOCK_Sim_wakeup);
+    CLOCK_EnableClock(kCLOCK_Debug);
+    CLOCK_EnableClock(kCLOCK_Dram);
+    CLOCK_EnableClock(kCLOCK_Sec_Debug);
+
+    /* Update core clock */
+    //SystemCoreClockUpdate();
 }
 
-static int nxp_mimx8mm6_init(const struct device *arg)
+static void SOC_UartInit(void)
+{
+#ifdef CONFIG_UART_MCUX_IUART
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(uart4), okay)
+	/* We need to grasp board uart exclusively */
+	rdc_periph_access_config_t uart4Config;
+	RDC_GetDefaultPeriphAccessConfig(&uart4Config);
+	uart4Config.periph = kRDC_Periph_UART4;
+	RDC_SetPeriphAccessConfig(RDC, &uart4Config);
+
+	/* Configure UART4 clock */
+	//CLOCK_SetRootMux(kCLOCK_RootUart4, kCLOCK_UartRootmuxSysPll1Div10);
+	//CLOCK_SetRootDivider(kCLOCK_RootUart4, 1U, 1U);
+	CLOCK_EnableClock(kCLOCK_Uart4);
+#endif
+
+#endif // CONFIG_UART_IMX
+}
+
+static void SOC_MemoryInit(void)
+{
+	// ERR050310: CM7 Icache/Dcache are not operational
+    if (SCB_CCR_IC_Msk == (SCB_CCR_IC_Msk & SCB->CCR))
+    {
+        SCB_DisableICache();
+    }
+    if (SCB_CCR_DC_Msk == (SCB_CCR_DC_Msk & SCB->CCR))
+    {
+        SCB_DisableDCache();
+    }
+
+    /* Disable MPU */
+    ARM_MPU_Disable();
+
+    /* MPU configure:
+     * Use ARM_MPU_RASR(DisableExec, AccessPermission, TypeExtField, IsShareable, IsCacheable, IsBufferable,
+     * SubRegionDisable, Size)
+     * API in mpu_armv7.h.
+     * param DisableExec       Instruction access (XN) disable bit,0=instruction fetches enabled, 1=instruction fetches
+     * disabled.
+     * param AccessPermission  Data access permissions, allows you to configure read/write access for User and
+     * Privileged mode.
+     *      Use MACROS defined in mpu_armv7.h:
+     * ARM_MPU_AP_NONE/ARM_MPU_AP_PRIV/ARM_MPU_AP_URO/ARM_MPU_AP_FULL/ARM_MPU_AP_PRO/ARM_MPU_AP_RO
+     * Combine TypeExtField/IsShareable/IsCacheable/IsBufferable to configure MPU memory access attributes.
+     *  TypeExtField  IsShareable  IsCacheable  IsBufferable   Memory Attribtue    Shareability        Cache
+     *     0             x           0           0             Strongly Ordered    shareable
+     *     0             x           0           1              Device             shareable
+     *     0             0           1           0              Normal             not shareable   Outer and inner write
+     * through no write allocate
+     *     0             0           1           1              Normal             not shareable   Outer and inner write
+     * back no write allocate
+     *     0             1           1           0              Normal             shareable       Outer and inner write
+     * through no write allocate
+     *     0             1           1           1              Normal             shareable       Outer and inner write
+     * back no write allocate
+     *     1             0           0           0              Normal             not shareable   outer and inner
+     * noncache
+     *     1             1           0           0              Normal             shareable       outer and inner
+     * noncache
+     *     1             0           1           1              Normal             not shareable   outer and inner write
+     * back write/read acllocate
+     *     1             1           1           1              Normal             shareable       outer and inner write
+     * back write/read acllocate
+     *     2             x           0           0              Device              not shareable
+     *  Above are normal use settings, if your want to see more details or want to config different inner/outter cache
+     * policy.
+     *  please refer to Table 4-55 /4-56 in arm cortex-M7 generic user guide <dui0646b_cortex_m7_dgug.pdf>
+     * param SubRegionDisable  Sub-region disable field. 0=sub-region is enabled, 1=sub-region is disabled.
+     * param Size              Region size of the region to be configured. use ARM_MPU_REGION_SIZE_xxx MACRO in
+     * mpu_armv7.h.
+     */
+
+    /* Region 0 [0x0000_0000 - 0x4000_0000] : Memory with Device type, not executable, not shareable, non-cacheable. */
+    MPU->RBAR = ARM_MPU_RBAR(0, 0x00000000U);
+    MPU->RASR = ARM_MPU_RASR(1, ARM_MPU_AP_FULL, 0, 0, 0, 1, 0, ARM_MPU_REGION_SIZE_1GB);
+
+    /* Region 1 TCML[0x0000_0000 - 0x0001_FFFF]: Memory with Normal type, shareable, non-cacheable */
+    MPU->RBAR = ARM_MPU_RBAR(1, 0x00000000U);
+    MPU->RASR = ARM_MPU_RASR(0, ARM_MPU_AP_FULL, 1, 1, 0, 0, 0, ARM_MPU_REGION_SIZE_128KB);
+
+    /* Region 2 QSPI[0x0800_0000 - 0x0FFF_FFFF]: Memory with Normal type, shareable, non-cacheable */
+    MPU->RBAR = ARM_MPU_RBAR(2, 0x08000000U);
+    MPU->RASR = ARM_MPU_RASR(0, ARM_MPU_AP_FULL, 1, 1, 0, 0, 0, ARM_MPU_REGION_SIZE_128MB);
+
+    /* Region 3 TCMU[0x2000_0000 - 0x2002_0000]: Memory with Normal type, shareable, non-cacheable */
+    MPU->RBAR = ARM_MPU_RBAR(3, 0x20000000U);
+    MPU->RASR = ARM_MPU_RASR(0, ARM_MPU_AP_FULL, 1, 1, 0, 0, 0, ARM_MPU_REGION_SIZE_128KB);
+
+    /* Region 4 DDR[0x4000_0000 - 0x8000_0000]: Memory with Normal type, shareable, non-cacheable */
+    MPU->RBAR = ARM_MPU_RBAR(4, 0x40000000U);
+    MPU->RASR = ARM_MPU_RASR(0, ARM_MPU_AP_FULL, 1, 1, 0, 0, 0, ARM_MPU_REGION_SIZE_1GB);
+
+    /* Region 5 DDR[0x8000_0000 - 0xF000_0000]: Memory with Normal type, shareable, non-cacheable */
+    MPU->RBAR = ARM_MPU_RBAR(5, 0x80000000U);
+    MPU->RASR = ARM_MPU_RASR(0, ARM_MPU_AP_FULL, 1, 1, 0, 0, 0, ARM_MPU_REGION_SIZE_1GB);
+
+    /*
+     * Enable MPU and HFNMIENA feature
+     * HFNMIENA ensures that M7 core uses MPU configuration when in hard fault, NMI, and FAULTMASK handlers,
+     * otherwise all memory regions are accessed without MPU protection, which has high risks of cacheable,
+     * especially for AIPS systems.
+     */
+    ARM_MPU_Enable(MPU_CTRL_PRIVDEFENA_Msk | MPU_CTRL_HFNMIENA_Msk);
+
+    /* Configure the force_incr programmable bit in GPV_5 of PL301_display, which fixes partial write issue.
+     * The AXI2AHB bridge is used for masters that access the TCM through system bus.
+     * Please refer to errata ERR050362 for more information */
+    *(uint32_t *)(GPV5_BASE_ADDR + FORCE_INCR_OFFSET) =
+        *(uint32_t *)(GPV5_BASE_ADDR + FORCE_INCR_OFFSET) | FORCE_INCR_BIT_MASK;
+}
+
+static int nxp_mimx8mn6_init(const struct device *arg)
 {
 	ARG_UNUSED(arg);
+
+	SOC_MemoryInit();
 
 	/* SoC specific RDC settings */
 	SOC_RdcInit();
@@ -138,7 +272,12 @@ static int nxp_mimx8mm6_init(const struct device *arg)
 	/* SoC specific Clock settings */
 	SOC_ClockInit();
 
+	/* Take over UART */
+	SOC_UartInit();
+
+	*(int*)0x308600B8 = 0;
+
 	return 0;
 }
 
-SYS_INIT(nxp_mimx8mm6_init, PRE_KERNEL_1, 0);
+SYS_INIT(nxp_mimx8mn6_init, PRE_KERNEL_1, 1);
