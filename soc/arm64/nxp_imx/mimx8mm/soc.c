571ed61eaa3adc696084e29af65b6cde08684c8c
Jiafei Pan <Jiafei.Pan@nxp.com>
2022-02-21 15:23:53 +0800

    soc: arm64: Add NXP i.MX8M Mini SoC support
    Add Zephyr support on i.MX8M Mini Cortex-A cores.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

---
+++ soc/arm64/nxp_imx/mimx8mm/soc.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2022 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <arch/cpu.h>
+#include <device.h>
+#include <init.h>
+#include <soc.h>
+
+/**
+ * @brief Perform basic hardware initialization at boot.
+ *
+ * This needs to be run from the very beginning.
+ * So the init priority has to be 0 (zero).
+ *
+ * @return 0
+ */
+static int mimx8mm_init(const struct device *arg)
+{
+	ARG_UNUSED(arg);
+
+	return 0;
+}
+
+SYS_INIT(mimx8mm_init, PRE_KERNEL_1, 0);
