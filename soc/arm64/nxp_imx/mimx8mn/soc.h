08fbab9edfa1a6e7664c4ff30bad0d1cabd6c266
Jiafei Pan <Jiafei.Pan@nxp.com>
2022-03-01 15:25:49 +0800

    soc: arm64: Add NXP i.MX8M Nano SoC support
    Add Zephyr support on i.MX8M Nano Cortex-A cores.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

---
+++ soc/arm64/nxp_imx/mimx8mn/soc.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2022 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC__H_
+#define _SOC__H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <sys/util.h>
+#include <toolchain.h>
+
+#ifndef _ASMLANGUAGE
+
+#include <devicetree.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SOC__H_ */
