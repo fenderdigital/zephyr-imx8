1000db037ffc51b2776603c369f08362ebecbea2
Jiafei Pan <Jiafei.Pan@nxp.com>
2022-04-28 12:14:38 +0800

    arm64: imx8mp: add SAI related dts and mapping
    Added SAI, I2C, audio blk related dts node and mmu mapping.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- soc/arm64/nxp_imx/mimx8mp/mmu_regions.c
+++ soc/arm64/nxp_imx/mimx8mp/mmu_regions.c
@@ -48,6 +48,56 @@ static const struct arm_mmu_region mmu_regions[] = {
 			      DT_REG_SIZE(DT_INST(1, nxp_imx_gpt)),
 			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),
 #endif
+
+	MMU_REGION_FLAT_ENTRY("IOMUXC",
+			      DT_REG_ADDR(DT_INST(0, nxp_imx_iomuxc)),
+			      DT_REG_SIZE(DT_INST(0, nxp_imx_iomuxc)),
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),
+
+	MMU_REGION_FLAT_ENTRY("GPC",
+			      DT_REG_ADDR(DT_INST(0, nxp_imx_gpc)),
+			      DT_REG_SIZE(DT_INST(0, nxp_imx_gpc)),
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),
+
+	MMU_REGION_FLAT_ENTRY("I2C3",
+			      DT_REG_ADDR(DT_INST(0, nxp_imx_i2c)),
+			      DT_REG_SIZE(DT_INST(0, nxp_imx_i2c)),
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),
+
+	MMU_REGION_FLAT_ENTRY("SAI1",
+			      DT_REG_ADDR(DT_INST(0, nxp_imx_sai)),
+			      DT_REG_SIZE(DT_INST(0, nxp_imx_sai)),
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),
+
+	MMU_REGION_FLAT_ENTRY("SAI2",
+			      DT_REG_ADDR(DT_INST(1, nxp_imx_sai)),
+			      DT_REG_SIZE(DT_INST(1, nxp_imx_sai)),
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),
+
+	MMU_REGION_FLAT_ENTRY("SAI3",
+			      DT_REG_ADDR(DT_INST(2, nxp_imx_sai)),
+			      DT_REG_SIZE(DT_INST(2, nxp_imx_sai)),
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),
+
+	MMU_REGION_FLAT_ENTRY("SAI5",
+			      DT_REG_ADDR(DT_INST(3, nxp_imx_sai)),
+			      DT_REG_SIZE(DT_INST(3, nxp_imx_sai)),
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),
+
+	MMU_REGION_FLAT_ENTRY("SAI6",
+			      DT_REG_ADDR(DT_INST(4, nxp_imx_sai)),
+			      DT_REG_SIZE(DT_INST(4, nxp_imx_sai)),
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),
+
+	MMU_REGION_FLAT_ENTRY("SAI7",
+			      DT_REG_ADDR(DT_INST(5, nxp_imx_sai)),
+			      DT_REG_SIZE(DT_INST(5, nxp_imx_sai)),
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),
+
+	MMU_REGION_FLAT_ENTRY("AUDIO_BLK_CTRL",
+			      DT_REG_ADDR(DT_INST(0, nxp_imx_audio_blk_ctrl)),
+			      DT_REG_SIZE(DT_INST(0, nxp_imx_audio_blk_ctrl)),
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),
 };
 
 const struct arm_mmu_config mmu_config = {
